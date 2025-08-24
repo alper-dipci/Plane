1ce2b790d63a74b32a8c3316d24aee14e5cbb068
alper dipci <dipcialper35@gmail.com>
2025-08-25 02:30:25 +0300

    saçma sapan kodlar

--- Assets/_Scripts/UpgradeTree/UIScrollTexture.cs
+++ Assets/_Scripts/UpgradeTree/UIScrollTexture.cs
@@ -3,22 +3,13 @@ using UnityEngine.UI;
 
 public class UIScrollTexture : MonoBehaviour
 {
-    [SerializeField] private Image image; // UI Image
+    [SerializeField] private RawImage image; // UI Image
     [SerializeField] private Vector2 scrollSpeed = new Vector2(0.1f, 0f); // X ve Y hız
-
-    private Material mat;
-
-    private void Awake()
-    {
-        // Image material’ının kopyasını alıyoruz, yoksa tüm UI Image’lar etkilenir
-        mat = Instantiate(image.material);
-        image.material = mat;
-    }
 
     private void Update()
     {
-        Vector2 offset = mat.mainTextureOffset;
-        offset += scrollSpeed * Time.deltaTime;
-        mat.mainTextureOffset = offset;
+        image.uvRect = new Rect(
+            image.uvRect.position + scrollSpeed * Time.deltaTime,
+            image.uvRect.size);
     }
 }
--- Assets/_Scripts/Upgrades/UpgradeNode.cs
+++ Assets/_Scripts/Upgrades/UpgradeNode.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using _Scripts.Enums;
 using Sirenix.OdinInspector;
 using UnityEngine;
@@ -10,14 +11,27 @@ namespace _Scripts.Upgrades
     {
         [SerializeField] private UpgradeNodeSO upgradeNodeSo;
         [SerializeField] private Button button;
+        [SerializeField] private Image lockedImage;
+        [SerializeField] private Image progressImage;
+        [SerializeField] private Image maxedImage;
 
-        [ReadOnly]
-        public int currentLevel;
+        [ReadOnly] public int currentLevel;
+        
+        public UpgradeNode parentNode;
+        public List<UpgradeNode> childNodes = new List<UpgradeNode>();
+        public List<Image> connectionLines; 
 
         private void Awake()
         {
             button.onClick.AddListener(OnButtonClicked);
+            UpdateVisual();
+            
+            if(parentNode != null && !parentNode.IsUnlocked())
+                HideNode();
+            else
+                ShowNode();
         }
+
         private void OnButtonClicked()
         {
             TryUpgrade();
@@ -28,25 +42,82 @@ namespace _Scripts.Upgrades
             // Check if we can upgrade
             if (currentLevel >= upgradeNodeSo.MaxLevel)
                 return;
-            
-            var upgradeData = upgradeNodeSo.GetUpgradeData(currentLevel +1);
+
+            var upgradeData = upgradeNodeSo.GetUpgradeData(currentLevel + 1);
             var cost = upgradeData.costData;
             // Check if we have enough currency
-            
+
             // Deduct currency
-            
+
             // upgrade our level
             currentLevel++;
-            
+
             // Apply the upgrade effect
             var effects = upgradeData.effects;
             foreach (var effect in effects)
                 effect.ApplyEffect();
+
+            //update visual
+            UpdateVisual();
+            
+            // If we just unlocked the first level, show child nodes
+            if (currentLevel == 1) 
+            {
+                foreach (var child in childNodes)
+                {
+                    child.ShowNode();
+                }
+            }
+        }
+
+        private void UpdateVisual()
+        {
+            var progressState = GetProgressState();
+            switch (progressState)
+            {
+                case UpgradeNodeProgressState.Locked:
+                    lockedImage.enabled = true;
+                    progressImage.fillAmount = 1;
+                    maxedImage.enabled = false;
+                    connectionLines.ForEach(line => line.enabled = false);
+                    break;
+                case UpgradeNodeProgressState.Maxed:
+                    lockedImage.enabled = false;
+                    progressImage.fillAmount = 0;
+                    maxedImage.enabled = true;
+                    connectionLines.ForEach(line => line.enabled = true);
+                    break;
+                case UpgradeNodeProgressState.Unlocked:
+                    lockedImage.enabled = false;
+                    progressImage.fillAmount = 1 - ((float)currentLevel / upgradeNodeSo.MaxLevel);
+                    maxedImage.enabled = false;
+                    connectionLines.ForEach(line => line.enabled = true);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
 
         private void OnDestroy()
         {
             button.onClick.RemoveListener(OnButtonClicked);
+        }
+        
+        public bool IsUnlocked()
+        {
+            return currentLevel > 0;
+        }
+
+        public void ShowNode()
+        {
+            gameObject.SetActive(true);
+            connectionLines.ForEach(line => line.enabled = true);
+            UpdateVisual();
+        }
+        public void HideNode()
+        {
+            gameObject.SetActive(false);
+            connectionLines.ForEach(line => line.enabled = false);
         }
 
         public UpgradeNodeProgressState GetProgressState()
